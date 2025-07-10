be459d99bd86ddf1ebae831e914f4deade6530ca
mvmedeiros <mvbmedeiros@gmail.com>
2025-07-10 14:21:47 +0200

    Forces title case on username to ensure standardization.

--- backend/user/serializer.py
+++ backend/user/serializer.py
@@ -6,15 +6,12 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username']
 
-    def validate(self, attrs): # Validate the inputs to user creation. Name and type are required, and type must be one of the predefined choices.
-        if not attrs.get('username'):
-            raise serializers.ValidationError("Name is required.")
-
-        return attrs
+    def validate_username(self, value):
+        return value.title()
 
     def create(self, validated_data):
         user, created = User.objects.get_or_create(
-            username=validated_data.get('username'),
+            username = validated_data['username'],
         )
         self._was_created = created  # Flag for the view to return 201 Created if a new user was created
         return user
